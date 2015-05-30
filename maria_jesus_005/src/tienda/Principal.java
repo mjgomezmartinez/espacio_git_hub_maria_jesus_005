d679d70cbc6c9cc320a8c5be4da0d7f80294b661
mariajesusg <mariajesusg@gmail.com>
2015-05-30 09:42:50 +0200

    clase principal

--- maria_jesus_005/src/tienda/Principal.java
+++ maria_jesus_005/src/tienda/Principal.java
@@ -1,5 +1,22 @@
 package tienda;
 
 public class Principal {
+	
+	Clientes codCliente1=new Clientes();
+	Clientes codCliente2=new Clientes();
+	Clientes codCliente3=new Clientes();
+	
+	Productos codProducto1=new Productos();
+	Productos codProducto2=new Productos();
+	Productos codProducto3=new Productos();
+	
+	Tiendas codTienda1=new Tiendas();
+	Tiendas codTienda2=new Tiendas();
+	Tiendas codTienda3=new Tiendas();
+	
+	Vendedores codVendedor1=new Vendedores();
+	Vendedores codVendedor2=new Vendedores();
+	Vendedores codVendedor3=new Vendedores();
+
 
 }
